6f1e4c56adfe2454b23d1ffb22507581dbfb72ee
Maurice Mutua <94635472+AfroGit@users.noreply.github.com>
2023-03-07 21:53:15 +0000

    Prepared the pseudo code for the JavaScript that runs the calculator

--- calculator.js
+++ calculator.js
@@ -1,6 +1,8 @@
 // Required abilities of a calculator..
 // Take in inputs, numbers and operators
+// Accept decimals
 // Display all inputs as they are entered
 // Store the input
 // perform calculations on the inputs
-// Return a result
+// Return a result.
+// 
