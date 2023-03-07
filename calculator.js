c0266026230ac3680df08d489e770060a3d06970
Maurice Mutua <94635472+AfroGit@users.noreply.github.com>
2023-03-07 22:35:53 +0000

    linked JS to HTMl

--- calculator.js
+++ calculator.js
@@ -3,6 +3,20 @@
 // Accept decimals
 // Display all inputs as they are entered
 // Store the input
+// Store previous total as start of next operation
+// Prevent invalid inputs
 // perform calculations on the inputs
+// Clear button should clear all from the screen
 // Return a result.
-// 
+
+
+const keys = document.queryselector('.calculator-buttons');
+keys.addEventListener('click', event => {
+  const {target} = event;
+  const {value} = target;
+  if(!target.matches('button')){
+    return;
+  } else {
+    console.log(`${event}`)
+  }
+});
