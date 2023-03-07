008a44be58d458c24ea45f0362080cb211cf87c3
Maurice Mutua <94635472+AfroGit@users.noreply.github.com>
2023-03-07 21:50:51 +0000

    Js Starter on deck

---
+++ calculator.js
@@ -0,0 +1,6 @@
+// Required abilities of a calculator..
+// Take in inputs, numbers and operators
+// Display all inputs as they are entered
+// Store the input
+// perform calculations on the inputs
+// Return a result
