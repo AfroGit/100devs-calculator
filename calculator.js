3ec1548c2b27325bb4665029bdb5ef10385f9287
Maurice Mutua <94635472+AfroGit@users.noreply.github.com>
2023-03-15 00:31:37 +0000

    worked on the ability of the calculator to take in inputs, numbers and operators and also made a switch statement to sort through the none numerical inputs of the calc.

--- calculator.js
+++ calculator.js
@@ -10,13 +10,80 @@
 // Return a result.
 
 
-const keys = document.queryselector('.calculator-buttons');
+const keys = document.querySelector('.calculator-buttons');
 keys.addEventListener('click', event => {
-  const {target} = event;
-  const {value} = target;
-  if(!target.matches('button')){
+  const { target } = event;
+  const { value } = target;
+  if (!target.matches('button')) {
     return;
   } else {
-    console.log(`${event}`)
+    calculator.parseInput(value)
+    // console.log(`${value}`)
   }
 });
+
+
+const calculator = {
+  displayText: '0',
+  prevTotal: null,
+
+
+  parseInput(value){
+    if(this.displayText === '0'){
+      this.displayValue = '';
+    }
+        // sort through "special buttons" to check if  clicked.
+    switch(value){
+      case '=':
+       // calculate answer.
+        break;
+        case 'AC':
+        // clear screen plus stored values
+        break;
+        case '.':
+        if(this.displayText == 0){
+          this.addText('0.')
+          // pass '0.' into add text method.
+        } else {
+          this.addText(value);
+          // add value into text string.
+        }
+        break;
+      default:
+        this.addText(value);
+        // add value to text string.
+        break;
+
+        
+
+
+
+        
+    }
+    
+  },
+
+  addText(value){
+    if(this.displayText === '0'){
+      this.displayValue = '';
+    } else if(this.prevTotal !== null){
+      this.displayText = this.prevTotal
+      this.prevTotal = null
+    }
+    if(isNaN(+(value)) && isNaN(+(this.displayText))){
+      if(isNaN(this.displayText.slice(-1))){
+        return;
+        
+      }
+      
+    }
+    this.displayText += value;
+    this.outputText(this.displayText)
+  },
+
+  outputText(text){
+    document.querySelector('.calculator-screen').value = text
+  },
+  
+};
+//
